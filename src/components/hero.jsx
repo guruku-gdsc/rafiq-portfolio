e93f56a0ac628c5f9819c446c166ad859f2fb646
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-06-25 11:25:29 +0700

    feat: add hero section

---
+++ src/components/hero.jsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <div className="wrapper flex flex-col gap-4">
+      <p className="font-hellix-bold text-8xl font-bold">
+        RAFIQ AL HAFIZH ADHA
+      </p>
+      <p className="text-xl">
+        A UI/UX Designer based in Medan, Indonesia. I'm available to explore new
+        projects and inspire through my work.
+      </p>
+    </div>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,22 @@
+import { Hero } from "@/components";
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <main className="p-4">
-      <p className="text-2xl font-bold">Hi mom!</p>
-    </main>
+    <>
+      <Head>
+        <title>Rafiq | UI/UX Designer</title>
+        <meta
+          name="description"
+          content="A UI/UX Designer based in Medan, Indonesia. I'm available to explore new
+          projects and inspire through my work."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/svg/logo.svg" />
+      </Head>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
